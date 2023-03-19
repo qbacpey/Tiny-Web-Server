7eeea608613ee39abd8829dd32e75bf8694df8b0
qbacpey <qbacpey@foxmail.com>
2023-03-19 20:47:16 +0800

    上传代码

---
+++ README.md
@@ -0,0 +1,54 @@
+# Tiny Web Server
+
+使用Epoll进行实现的简单Web服务器
+
+## 代码设计
+
+![image-20230319201257171](D:\Computer Science\NEU\Linux操作系统\实验\20206691\代码\lab3\src\README.assets\image-20230319201257171.png)
+
+### 并行性
+
+本项目的并行性通过线程池（Posix线程）以及epoll进行实现
+
+#### 执行流程
+
+1. 主线程执行初始化工作（配置网络、监听端口等）；
+
+2. 主线程创建epoll实例，将需要监听的文件描述符添加到epoll的兴趣列表中；
+
+3. 主线程创建`THREAD_NUM`个线程（`epollfd`以及`listenfd`会被作为参数传递），随后主线程和子线程都将作为工作线程存在；
+
+4. 所有工作线程在epoll实例上调用`epoll_wait`函数；
+
+5. 当任何事件被捕捉到时，工作线程会执行下列操作：
+
+   1. 检查`fd`是否为`listenfd`，如果是，那么对其调用`accept`并将`connfd`（`accept`的返回值）添加到兴趣列表中；
+
+   2. 如果不是，那么`fd`就是`connfd`，此时可以从中读取数据。如果读取完成（检测到新的一行），那么响应此请求。否则将读取状态保存到`event.data.ptr`中
+
+      读取状态的定义如下：
+
+      ~~~c
+      typedef struct HttpStatus {
+          int connfd; // 连接文件描述符
+          char *header; // 已读取的HTTP请求头
+          size_t readn; // 已读取的字节数
+          FILE *file; // 需要发送的文件
+          size_t left; // 尚需发送的文件
+          req_status_t req_status;
+      } http_status_t;
+      ~~~
+
+      `req_status_t`被定义为：
+
+      ~~~c
+      typedef enum REQUEST_STATUS {
+          Reading,
+          Writing,
+          Ended
+      } req_status_t;
+      ~~~
+
+      下一次`epoll_wait`接收到此`fd`中的事件时，服务器会继续处理此请求
+
+   3. 请求头读取完毕之后，`connfd`会进入`Writing`状态。如果`sendfile`函数发送数据时触发了`EAGAIN`事件，同时`left > 0`，那么意味着写端暂时不可用。保存当前状态到`HttpStatus`中之后，利用`EPOLL_CTL_MOD`将`connfd`以`EPOLLOUT | EPOLLET`重新装配到epoll实例中；
---
+++ main.c
@@ -0,0 +1,360 @@
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/sendfile.h>
+#include "netutils.h"
+#include "main.h"
+
+#define PORT 8000
+
+#define min(a, b) ((a) < (b) ? (a) : (b))
+
+int parse_request(const char *req_str, request_t *req_info) {
+    if (sscanf(req_str, "%s %s %[^\r\n]", req_info->method, req_info->uri, req_info->version) != 3) {
+        fprintf(stderr, "malformed http request\n");
+        return -1;
+    }
+}
+
+FILE *handle_request(const request_t *req) {
+    if (strncmp(req->method, "GET", 3) != 0) {
+        fprintf(stderr, "only support `GET` method\n");
+        return NULL;
+    }
+
+    char *abs_path = (char *)malloc(PATH_MAX);
+    if (abs_path == NULL) {
+        perror("malloc");
+    }
+    const char *rel_path = req->uri[0] == '/' ? req->uri + 1 : req->uri;
+    char *cur_dir = (char *)malloc(PATH_MAX);
+    if (cur_dir == NULL) {
+        perror("malloc");
+    }
+
+    realpath(rel_path, abs_path);
+    getcwd(cur_dir, PATH_MAX);
+
+    if (strncmp(cur_dir, abs_path, strlen(cur_dir)) != 0) {
+        send_response(req->connfd, ISE, content_500, strlen(content_500));
+        return NULL;
+    } 
+
+    FILE *file = fopen(abs_path, "rb");
+    free(abs_path);
+    free(cur_dir);
+    if (file == NULL) {
+        if (errno == ENOENT) {
+            send_response(req->connfd, NF, content_404, strlen(content_404));
+        } else {
+            send_response(req->connfd, ISE, content_500, strlen(content_500));
+        }
+        return NULL;
+    }
+
+    return file;
+}
+
+void send_response(int connfd, status_t status, 
+                   const char *content, size_t content_length) {
+    char buf[128];
+
+    if (status == NF) {
+        sprintf(buf, "HTTP/1.0 404 Not Found\r\n");
+    } else if (status == OK) {
+        sprintf(buf, "HTTP/1.0 200 OK\r\n");
+    } else {
+        sprintf(buf, "HTTP/1.0 500 Internal Servere Error\r\n");
+    }
+
+    sprintf(buf, "%sContent-Length: %lu\r\n\r\n", buf, content_length);
+
+    size_t buf_len = strlen(buf);
+
+    if (rio_writen(connfd, buf, buf_len) < buf_len) {
+        fprintf(stderr, "error while sending response\n");
+        return;
+    }
+    if (rio_writen(connfd, content, content_length) < content_length) {
+        fprintf(stderr, "error while sending response\n");
+    }
+}
+
+void send_file_response(int connfd, FILE *file) {
+    fseek(file, 0L, SEEK_END);
+    __off_t file_size = ftell(file);
+    fseek(file, 0L, SEEK_SET);
+    
+    char header[64];
+    sprintf(header, "HTTP/1.0 200 OK\r\nContent-Length: %ld\r\n\r\n", file_size);
+    rio_writen(connfd, header, strlen(header));
+
+    size_t readn;
+
+    int filefd = fileno(file);
+    ssize_t left = file_size;
+    ssize_t writen;
+    while (left > 0) {
+        writen = sendfile(connfd, filefd, NULL, left);
+        if (writen < 0) {
+            if (errno == EAGAIN || errno == EWOULDBLOCK)
+                continue;
+            perror("sendfile");
+            break;
+        }
+        left -= writen;
+    }
+}
+
+void server(http_status_t *status) {
+    int connfd = status->connfd;
+    char *header = status->header;
+    size_t readn = status->readn;
+
+    if (status->req_status == Reading) {
+        int is_end = 0;
+
+        while (1) {
+            ssize_t ret = read(connfd, header + readn, 1);
+            if (readn >= MAX_HEADER) {
+                // 内容过多
+                status->req_status = Ended;
+                send_response(connfd, ISE, content_500, sizeof(content_500));
+                return;
+            }
+            if (ret < 0) {
+                if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                    // 读取结束
+                    break;
+                } else {
+                    perror("read");
+                    break;
+                }
+            } else if (ret == 0) {
+                // EOF
+                break;
+            } else {
+                readn++;
+                if ( readn >= 4
+                && header[readn - 1] == '\n'
+                && header[readn - 2] == '\r'
+                && header[readn - 3] == '\n'
+                && header[readn - 4] == '\r' ) {
+                    is_end = 1;
+                    break;
+                }
+            }
+        }
+
+        if (!is_end) {
+            status->readn = readn;
+            return;
+        }
+
+        request_t req_info;
+        if (parse_request(header, &req_info) < 0) {
+            send_response(connfd, ISE, content_500, strlen(content_500));
+            status->req_status = Ended;
+            return;
+        }
+
+        req_info.connfd = connfd;
+        FILE *file = handle_request(&req_info);
+
+        fseek(file, 0L, SEEK_END);
+        __off_t file_size = ftell(file);
+        fseek(file, 0L, SEEK_SET);
+
+        if (file != NULL) {
+            char resp_header[64];
+            sprintf(resp_header, "HTTP/1.0 200 OK\r\nContent-Length: %ld\r\n\r\n", file_size);
+            rio_writen(connfd, resp_header, strlen(resp_header));
+            status->file = file;
+            status->left = file_size;
+            status->req_status = Writing;
+        } else {
+            status->req_status = Ended;
+            return;
+        }
+    }
+
+    if (status->req_status == Writing) {
+        FILE *file = status->file;
+        int filefd = fileno(file);
+        size_t left = status->left;
+        ssize_t writen;
+
+        while (left > 0) {
+            writen = sendfile(connfd, filefd, NULL, left);
+            if (writen < 0) {
+                if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                    // read end normally
+                    break;
+                } else {
+                    perror("sendfile");
+                    status->req_status = Ended;
+                    return;
+                }
+            } else if (writen == 0) {
+                // EOF encountered
+                status->req_status = Ended;
+                return;
+            } else {
+                left -= writen;
+            }
+        }
+
+        if (left == 0) {
+            status->req_status = Ended;
+        } else {
+            status->left = left;
+        }
+    }
+}
+
+void *thread(void *args) {
+    struct epoll_event *events = (struct epoll_event *)malloc(sizeof(struct epoll_event) * MAX_EVENTS);
+    if (events == NULL) {
+        perror("malloc");
+    }
+    struct epoll_event ev;
+    int epollfd = ((struct thread_args*)args)->epollfd;
+    int listenfd = ((struct thread_args*)args)->listenfd;
+
+    struct sockaddr_in clnt_addr;
+    socklen_t clnt_addr_len = sizeof(clnt_addr);
+
+    int nfds;
+    while (1) {
+        nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
+
+        if (nfds <= 0) {
+            perror("epoll_wait");
+            continue;
+        }
+        for (int n = 0; n < nfds; ++n) {
+            if (events[n].data.fd == listenfd) {
+                while (1) {
+                    int connfd = accept(listenfd, (struct sockaddr *)&clnt_addr, &clnt_addr_len);
+                    if (connfd < 0) {
+                        if (errno == EAGAIN | errno == EWOULDBLOCK) {
+                            break;
+                        } else {
+                            perror("accept");
+                            break;
+                        }
+                    }
+
+                    setnonblocking(connfd);
+
+                    ev.events = EPOLLIN | EPOLLET | EPOLLONESHOT;
+
+                    http_status_t *status = (http_status_t *)malloc(sizeof(http_status_t));
+                    char *header = (char *)malloc(MAX_HEADER);
+                    if (header == NULL || status == NULL) {
+                        perror("malloc");
+                        exit(1);
+                    }
+                    status->header = header;
+                    status->connfd = connfd;
+                    status->readn = 0;
+                    status->req_status = Reading;
+                    ev.data.ptr = status;
+
+                    if (epoll_ctl(epollfd, EPOLL_CTL_ADD, connfd, &ev) < 0) {
+                        perror("epoll_ctl");
+                        continue;
+                    }
+                }
+            } else {
+                http_status_t *status = (http_status_t *)events[n].data.ptr;
+                server(status);
+                if (status->req_status == Reading) {
+                    ev.events = EPOLLIN | EPOLLET | EPOLLONESHOT;
+                    ev.data.ptr = status;
+                    if (epoll_ctl(epollfd, EPOLL_CTL_MOD, status->connfd, &ev) < 0) {
+                        perror("epoll_ctl");
+                        continue;
+                    }
+                } else if (status->req_status == Writing) {
+                    ev.events = EPOLLOUT | EPOLLET | EPOLLONESHOT;
+                    ev.data.ptr = status;
+                    if (epoll_ctl(epollfd, EPOLL_CTL_MOD, status->connfd, &ev) < 0) {
+                        perror("epoll_ctl");
+                        continue;
+                    }
+                } else if (status->req_status == Ended) {
+                    if (status->file != NULL) {
+                        fclose(status->file);
+                        status->file = NULL;
+                    }
+                    if (status->header != NULL) {
+                        free(status->header);
+                        status->header = NULL;
+                    }
+                    close(status->connfd);
+                    free(status);
+                }
+            }
+        }
+    }
+
+    free(events);
+}
+
+void setnonblocking(int fd) {
+    int old_option = fcntl(fd, F_GETFL);
+    int new_option = old_option | O_NONBLOCK;
+      
+    fcntl(fd, F_SETFL, new_option);
+}
+
+int main() {
+    signal(SIGPIPE, SIG_IGN);
+
+    int listenfd = open_listenfd(PORT);
+
+    setnonblocking(listenfd);
+
+    pthread_t threads[THREAD_NUM];
+    
+    int epollfd = epoll_create1(0);
+    if (epollfd < 0) {
+        fprintf(stderr, "error while creating epoll fd\n");
+        exit(1);
+    }
+
+    struct thread_args targs;
+    targs.epollfd = epollfd;
+    targs.listenfd = listenfd;
+
+    struct epoll_event epevent;
+    epevent.events = EPOLLIN | EPOLLET;
+    epevent.data.fd = listenfd;
+
+    if (epoll_ctl(epollfd, EPOLL_CTL_ADD, listenfd, &epevent) < 0) {
+        fprintf(stderr, "error while adding listen fd to epoll inst\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < THREAD_NUM; ++i) {
+        if (pthread_create(&threads[i], NULL, thread, &targs) < 0) {
+            fprintf(stderr, "error while creating %d thread\n", i);
+            exit(1);
+        }
+    }
+
+    thread(&targs);
+
+    close(epollfd);
+    close(listenfd);
+}
